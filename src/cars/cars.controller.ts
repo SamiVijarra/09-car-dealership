78380b5a1bc9f5960407d4d265426be9c92d723c
Sami <samivijarra1@gmail.com>
2026-04-28 14:41:43 -0300

    feat: add get info by id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,22 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = [
+    {
+      id: 1,
+      make: 'Toyota',
+      model: 'Camry',
+      year: 2020,
+    },
+    {
+      id: 2,
+      make: 'Honda',
+      model: 'Civic',
+      year: 2019,
+    },
+  ];
   @Get()
   getAllCars() {
-    return [
-      {
-        id: 1,
-        make: 'Toyota',
-        model: 'Camry',
-        year: 2020,
-      },
-      {
-        id: 2,
-        make: 'Honda',
-        model: 'Civic',
-        year: 2019,
-      },
-    ];
+    return this.cars;
+  }
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({id});
+    return this.cars[id];
   }
 }
