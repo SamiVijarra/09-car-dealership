ed3c8294fdb42f972fbf1fb1397d38581b853a61
Sami <samivijarra1@gmail.com>
2026-04-29 11:39:53 -0300

    feat: add create new car

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -9,19 +10,16 @@ export class CarsService {
       id: uuid(),
       brand: 'Toyota',
       model: 'Camry',
-      year: 2020,
     },
     {
       id: uuid(),
       brand: 'Honda',
       model: 'Civic',
-      year: 2019,
     },
     {
       id: uuid(),
       brand: 'Jeep',
       model: 'Wrangler',
-      year: 2021,
     },
   ];
   findAll() {
@@ -31,5 +29,13 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
     return car;
+  }
+  create(createCarDto: CreateCarDto) {
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(newCar);
+    return newCar;
   }
 }
